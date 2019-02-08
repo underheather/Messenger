7338881491d0786771f20203615fa0037ddf2cfd
Heather Boa Min <heather.bmin@gmail.com>
2019-02-09 00:34:21 +0800

    changed app.tsx
    - changed bg color
    - added msg box

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default class App extends React.Component {
+  state = {
+    textbox: " ",
+  };
+  handleTextbox = () => {
+    this.setState({ Textbox: Text });
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Write something here"
+          onChangeText={this.handleTextbox}
+        />
       </View>
     );
   }
@@ -14,8 +24,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#5B5655",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-end",
+  },
+  input: {
+    marginBottom: 40,
+    paddingLeft: 15,
+    height: 40,
+    borderRadius: 20,
+    width: "90%",
+    backgroundColor: "#FFFFFF",
   },
 });
